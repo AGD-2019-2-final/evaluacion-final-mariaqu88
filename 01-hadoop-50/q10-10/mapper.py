7faf0e50bd8ef520167aa0c92807e6c4d5a5dbfd
maria <mgomezper@unal.edu.co>
2020-01-25 22:34:52 -0500

    punto10

--- 01-hadoop-50/q10-10/mapper.py
+++ 01-hadoop-50/q10-10/mapper.py
@@ -2,4 +2,9 @@ import sys
 #
 # >>> Escriba el codigo del mapper a partir de este punto <<<
 #
-        
+
+if __name__ == "__main__":
+    for line in sys.stdin:
+      key_columna = line.split('\t')[0]
+      valor_columna = line.split('\t')[1].strip()
+      sys.stdout.write("{}\t{}\n".format(key_columna,valor_columna))       
--- 01-hadoop-50/q10-10/reducer.py
+++ 01-hadoop-50/q10-10/reducer.py
@@ -2,3 +2,38 @@ import sys
 #
 # >>> Escriba el codigo del mapper a partir de este punto <<<
 #
+
+if __name__ == '__main__':
+    lista = []
+    curkey = None
+    listaNum = ''
+    for line in sys.stdin:
+        key, val = line.split("\t")
+        
+        for letter in val.split(','):
+            lista.append([letter.strip(),int(key)])
+    
+    lista = sorted(lista, key=lambda x: (x[0],x[1]))
+    for line in lista:
+        
+        if curkey is None:
+            curkey = line[0]
+        #print(str(line[1]) +'-' +  line[0] +'=='+ curkey)
+        if line[0] == curkey:
+            if line[1] not in listaNum.split(','):
+                if listaNum == '':
+                    listaNum += str(line[1])
+                else:
+                    listaNum += ',' + str(line[1])
+        else:
+            sys.stdout.write("{}\t{}\n".format(curkey, listaNum))
+            listaNum = ''
+            
+            if listaNum == '':
+                listaNum += str(line[1])
+            else:
+                listaNum += ',' + str(line[1])
+            
+            curkey = line[0]
+    
+    sys.stdout.write("{}\t{}\n".format(curkey, listaNum))
