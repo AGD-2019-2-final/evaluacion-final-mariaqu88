a5cfc19c07d7169851724e999fa8a142c05329bc
maria <mgomezper@unal.edu.co>
2020-01-25 21:40:30 -0500

    punto5

--- 01-hadoop-50/q05-10/mapper.py
+++ 01-hadoop-50/q05-10/mapper.py
@@ -2,3 +2,9 @@ import sys
 #
 #  >>> Escriba el codigo del mapper a partir de este punto <<<
 #
+import sys
+if __name__ == "__main__":
+    for line in sys.stdin:
+      key_columna = line.split('   ')[1].split('-')[1]
+      valor_columna = 1
+      sys.stdout.write("{}\t{}\n".format(key_columna,valor_columna))
