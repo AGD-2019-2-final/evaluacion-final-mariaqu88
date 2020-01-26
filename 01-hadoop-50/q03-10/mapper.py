dc351ac165f09ab150971492990cae30f9d88bdd
maria <mgomezper@unal.edu.co>
2020-01-25 21:29:37 -0500

    punto3

--- 01-hadoop-50/q03-10/mapper.py
+++ 01-hadoop-50/q03-10/mapper.py
@@ -2,3 +2,10 @@ import sys
 #
 # >>> Escriba el codigo del mapper a partir de este punto <<<
 #
+
+if __name__ == "__main__":
+    for line in sys.stdin:
+      line = line.strip()
+      key_columna = line.split(',')[1]
+      valor_columna = line.split(',')[0]
+      sys.stdout.write("{},{}\n".format(key_columna,valor_columna))
--- 01-hadoop-50/q03-10/reducer.py
+++ 01-hadoop-50/q03-10/reducer.py
@@ -2,3 +2,9 @@ import sys
 #
 # >>> Escriba el codigo del reducer a partir de este punto <<<
 #
+if __name__ == '__main__':
+    for line in sys.stdin:
+      line = line.strip()
+      key_columna = line.split(',')[1]
+      valor_columna = line.split(',')[0]
+      sys.stdout.write("{},{}\n".format(key_columna,valor_columna))
