2e0e2d2178204ed350e87753870e737622f408cc
maria <mgomezper@unal.edu.co>
2020-01-25 22:22:35 -0500

    punto7

--- 01-hadoop-50/q07-10/mapper.py
+++ 01-hadoop-50/q07-10/mapper.py
@@ -2,3 +2,9 @@ import sys
 #
 #  >>> Escriba el codigo del mapper a partir de este punto <<<
 #
+if __name__ == "__main__":
+    for line in sys.stdin:
+      key_columna = line.split('   ')[0]
+      valor1_columna = line.split('   ')[1]
+      valor2_columna = line.split('   ')[2].strip()
+      sys.stdout.write("{}\t{}\t{}\n".format(key_columna,valor1_columna,valor2_columna))
--- 01-hadoop-50/q07-10/reducer.py
+++ 01-hadoop-50/q07-10/reducer.py
@@ -2,3 +2,12 @@ import sys
 #
 #  >>> Escriba el codigo del mapper a partir de este punto <<<
 #
+
+if __name__ == '__main__':
+    lista = []
+    for line in sys.stdin:
+        key, val1, val2 = line.split("\t")
+        lista.append([key,val1,int(val2.strip())])
+    lista = sorted(lista, key=lambda x: (x[0], x[2]))
+    for line in lista:
+        sys.stdout.write("{}\t{}\t{}\n".format(line[0], line[1], line[2]))
