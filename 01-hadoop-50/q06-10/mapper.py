d713656b9bd6c6910426d01dc9a25fba02245f3e
maria <mgomezper@unal.edu.co>
2020-01-25 22:13:05 -0500

    punto6

--- 01-hadoop-50/q06-10/mapper.py
+++ 01-hadoop-50/q06-10/mapper.py
@@ -2,4 +2,9 @@ import sys
 #
 #  >>> Escriba el codigo del mapper a partir de este punto <<<
 #
-
+import sys
+if __name__ == "__main__":
+    for line in sys.stdin:
+      key_columna = line.split('   ')[0]
+      valor_columna = line.split('   ')[2].strip()
+      sys.stdout.write("{}\t{}\n".format(key_columna,valor_columna))
--- 01-hadoop-50/q06-10/reducer.py
+++ 01-hadoop-50/q06-10/reducer.py
@@ -2,3 +2,29 @@ import sys
 #
 # >>> Escriba el codigo del mapper a partir de este punto <<<
 #
+if __name__ == '__main__':
+
+    curkey = None
+    maximo_valor = None
+    minimo_valor = None
+
+    for line in sys.stdin:
+        key, val = line.split("\t")
+        val = float(val)
+        
+        if maximo_valor is None:
+          maximo_valor = val
+        if minimo_valor is None:
+          minimo_valor = val
+
+        if key == curkey:
+              maximo_valor = max(maximo_valor,val)
+              minimo_valor = min(minimo_valor,val)
+        else:
+            if curkey is not None:
+                sys.stdout.write("{}\t{}\t{}\n".format(curkey, maximo_valor,minimo_valor))
+
+            curkey = key
+            maximo_valor = val
+            minimo_valor = val
+    sys.stdout.write("{}\t{}\t{}\n".format(curkey, maximo_valor,minimo_valor))
