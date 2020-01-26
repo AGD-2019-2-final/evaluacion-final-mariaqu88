60725df8f72d55ace121faab4d52b0d7ce5a0431
maria <mgomezper@unal.edu.co>
2020-01-25 21:21:53 -0500

    punto2

--- 01-hadoop-50/q02-10/mapper.py
+++ 01-hadoop-50/q02-10/mapper.py
@@ -2,3 +2,9 @@ import sys
 #
 # >>> Escriba el codigo del mapper a partir de este punto <<<
 #
+
+if __name__ == "__main__":
+    for line in sys.stdin:
+      key_columna = line.split(',')[3]
+      valor_columna = line.split(',')[4]
+      sys.stdout.write("{}\t{}\n".format(key_columna,valor_columna))
--- 01-hadoop-50/q02-10/reducer.py
+++ 01-hadoop-50/q02-10/reducer.py
@@ -2,3 +2,26 @@ import sys
 #
 #  >>> Escriba el codigo del reducer a partir de este punto <<<
 #
+if __name__ == '__main__':
+
+    curkey = None
+    total = None
+
+    for line in sys.stdin:
+        key, val = line.split("\t")
+        val = int(val)
+        
+        if total is None:
+          total = val
+
+        if key == curkey:
+              total = max(total,val)
+        else:
+            
+            if curkey is not None:
+               
+                sys.stdout.write("{}\t{}\n".format(curkey, total))
+
+            curkey = key
+            total = val
+    sys.stdout.write("{}\t{}\n".format(curkey, total))
